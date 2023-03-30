8ebf0eabc75e2b60e0de4b75a862df908b94c5c9
GuilhermeCesar029 <guicesar029@gmail.com>
2023-03-30 11:57:12 -0300

    Logica para dar play e pausar o video

--- src/pages/Video.js
+++ src/pages/Video.js
@@ -1,11 +1,32 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import './video.css'
 
 function Video() {
+
+    const videoRef = useRef(null)
+    const [play, setPlay] = useState(false)
+
+    function handdleStart(){
+
+        if(play == false){
+            videoRef.current.play()
+            setPlay(true)
+        }
+
+        if(play == true){
+            videoRef.current.pause()
+            setPlay(false)
+        }
+        
+    }
+
     return (
         <div className='video'>
             <video 
                 className='video__player' 
+                ref={videoRef}
+                onClick={handdleStart}
+                loop
                 src="https://firebasestorage.googleapis.com/v0/b/jornada2-eb156.appspot.com/o/ZqU6oFX6.mp4.mp4?alt=media&token=9839e872-2d5e-4da3-9299-17eb2949831d"
             >
                 
