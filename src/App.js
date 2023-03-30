f9e7424e76969b1a17f432330b0840b7341df29d
GuilhermeCesar029 <guicesar029@gmail.com>
2023-03-30 11:15:17 -0300

    Criando componente para video e adicionando scroll

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import Video from './pages/Video';
 
 function App() {
   return (
     <div className="App">
-      
+      <div className='app__videos'>
+        <Video/>
+        <Video/>
+      </div>
     </div>
   );
 }
---
+++ src/pages/Video.js
@@ -0,0 +1,13 @@
+import React from "react";
+import './video.css'
+
+function Video() {
+    return (
+        <div className='video'>
+            Componente
+            <video className='video__player'></video>
+        </div>
+    )
+}
+
+export default Video
