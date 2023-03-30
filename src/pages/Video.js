24a6c651fedbff33f65772c5941ee37199c677b6
GuilhermeCesar029 <guicesar029@gmail.com>
2023-03-30 11:45:28 -0300

    Colocando link do video

--- src/pages/Video.js
+++ src/pages/Video.js
@@ -4,8 +4,12 @@ import './video.css'
 function Video() {
     return (
         <div className='video'>
-            Componente
-            <video className='video__player'></video>
+            <video 
+                className='video__player' 
+                src="https://firebasestorage.googleapis.com/v0/b/jornada2-eb156.appspot.com/o/ZqU6oFX6.mp4.mp4?alt=media&token=9839e872-2d5e-4da3-9299-17eb2949831d"
+            >
+                
+            </video>
         </div>
     )
 }
