d920b5f35ae5afcbac2978c22e83b21ff403d386
GuilhermeCesar029 <guicesar029@gmail.com>
2023-03-31 10:13:21 -0300

    Colocando textos no footer e vinil

--- src/pages/components/footer/VideoFooter.js
+++ src/pages/components/footer/VideoFooter.js
@@ -3,7 +3,21 @@ import "./videoFooter.css"
 
 function VideoFooter() {
     return (
-        <div className='videoFooter'>VideoFooter</div>
+        <div className='videoFooter'>
+            <div className='videoFooter__text'>
+                <h3>@GuilhermeCesar029</h3>
+                <p>Descrição do Video</p>
+                <div className='videoFooter__music'>
+                    <p>Icone</p>
+                    <p>Titulo da musica</p>
+                </div>                
+            </div>
+            <img 
+                className='videoFooter__record' 
+                alt='Imagem de um vinil girando' 
+                src='https://poqlymuephttfsljdabn.supabase.co/storage/v1/object/public/jornadadev/vinil.png' 
+            />
+        </div>
     )
 }
 
