31a1575820856755c4d44bb81cebdcbb144bbb0e
GuilhermeCesar029 <guicesar029@gmail.com>
2023-03-31 23:14:15 -0300

    Passando dados pelo props

--- src/App.js
+++ src/App.js
@@ -5,8 +5,24 @@ function App() {
   return (
     <div className="App">
       <div className='app__videos'>
-        <Video/>
-        <Video/>
+        <Video
+          likes={100}
+          messages={200}
+          shares={300}
+          name="GuilhermeCesar029"
+          description="Descrição do Video" 
+          music="Musica"
+          url="https://poqlymuephttfsljdabn.supabase.co/storage/v1/object/public/jornadadev/brecker2.mp4"
+        />
+        <Video
+          likes={400}
+          messages={200}
+          shares={500}
+          name="GuilhermeCesar029"
+          description="Descrição do Video" 
+          music="Musica" 
+          url="https://poqlymuephttfsljdabn.supabase.co/storage/v1/object/public/jornadadev/brecker2.mp4"
+        />
       </div>
     </div>
   );
